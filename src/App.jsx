71f3f6d6162a195b4c081b490e9587e3b3cd95f3
Martin del Pino <119134824+dellpinos@users.noreply.github.com>
2024-01-18 00:58:17 -0300

    iniciando vistas del proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,15 @@ function App() {
 
 
     return (
-        <>
+        <div className=" container mx-auto mt-20">
             <Header/>
+
+            <div className="mt-12 md:flex">
             <Formulario />
             <ListadoPacientes />
-        </>
+
+            </div>
+        </div>
     )
 }
 
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,11 @@
 
-export default function ListadoPacientes() {
-
+ const ListadoPacientes = () => {
   return (
-    <h2>Listado Pacientes</h2>
+    <div className="w-1/2 lg:w-3/5">
+    <h2 >ListadoPacientes</h2>
+
+    </div>
   )
 }
+
+export default ListadoPacientes;
